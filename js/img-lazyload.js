454f6f1ff5813909c9307e5c53687ad0e713c824
Hanyin <mundi.xu@gmail.com>
2025-08-25 14:22:34 +0800

    Site updated: 2025-08-25 14:22:33

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -21,33 +21,72 @@
   // 图片懒加载优化
   function lazyLoadImages() {
     var lazyImages = document.querySelectorAll('img[lazyload]');
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          var img = entry.target;
-          // 获取实际图片地址
-          var src = img.getAttribute('data-src');
-          var srcset = img.getAttribute('data-srcset');
-          
-          if (src) {
-            img.src = src;
+    if ('IntersectionObserver' in window) {
+      // 获取配置中的offset_factor，如果没有则使用默认值2
+      var offsetFactor = (CONFIG && CONFIG.lazyload && CONFIG.lazyload.offset_factor) || 2;
+      var rootMargin = (window.innerHeight || document.documentElement.clientHeight) * offsetFactor + 'px';
+      
+      var imageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            var img = entry.target;
+            loadImage(img);
+            observer.unobserve(img);
           }
-          if (srcset) {
-            img.srcset = srcset;
-          }
-          
-          img.removeAttribute('lazyload');
-          img.classList.add('lazyloaded');
-          observer.unobserve(img);
-        }
+        });
+      }, {
+        rootMargin: rootMargin
       });
-    }, {
-      rootMargin: (window.innerHeight || document.documentElement.clientHeight) * (CONFIG.lazyload.offset_factor || 2) + 'px'
-    });
 
-    lazyImages.forEach(function(img) {
-      imageObserver.observe(img);
-    });
+      lazyImages.forEach(function(img) {
+        imageObserver.observe(img);
+      });
+    } else {
+      // 降级处理：直接加载所有图片
+      lazyImages.forEach(loadImage);
+    }
+  }
+
+  // 加载单个图片
+  function loadImage(img) {
+    // 获取实际图片地址
+    var src = img.getAttribute('data-src');
+    var srcset = img.getAttribute('data-srcset');
+    
+    // 创建新的图片对象来预加载
+    if (src || srcset) {
+      var newImg = new Image();
+      
+      newImg.onload = function() {
+        if (src) {
+          img.src = src;
+        }
+        if (srcset) {
+          img.srcset = srcset;
+        }
+        
+        img.removeAttribute('lazyload');
+        img.classList.add('lazyloaded');
+      };
+      
+      // 如果有错误，也移除lazyload属性避免图片一直不显示
+      newImg.onerror = function() {
+        img.removeAttribute('lazyload');
+        img.classList.add('lazyload-error');
+      };
+      
+      // 设置预加载图片的源
+      if (src) {
+        newImg.src = src;
+      }
+      if (srcset) {
+        newImg.srcset = srcset;
+      }
+    } else {
+      // 如果没有data-src或data-srcset，直接移除lazyload属性
+      img.removeAttribute('lazyload');
+      img.classList.add('lazyloaded');
+    }
   }
 
   // 初始化
